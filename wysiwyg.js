b1db8a34ce148e01e2c190b16785bdb732771d8e
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-04-26 16:04:01 -0500

    cleaned up needs work

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,45 +1,40 @@
-var zepp = [
-{
-  title: "Lead Guitar",
-  name: "Jimmy Page",
-  bio: "Playing guitar while a acitve member in the cult of pony tail enthusiast.",
-  image: "http://www.theglobeandmail.com/arts/music/article25802019.ece/BINARY/w940/image.jpg",
-  lifespan: {
-    birth: 1957,
-    death: 2345
-  }
-},
-{
-  title: "Lead Singer",
-  name: "Robert Plant",
-  bio: "Singing classic rock songs and wearing shirts that shows a bounty of chest hair.",
-  image: "http://static.tumblr.com/6741800edc5bac8e1649df8b2901420d/a38zs7m/019mx47u2/tumblr_static_robert_plant02.jpg",
-  lifespan: {
-    birth: 1965,
-    death: 2200
-  }
-},
-{
-  title: "Bass Player",
-  name: "John Paul Jones",
-  bio: "Rockin the bass and a Lord of the Rings extra.",
-  image: "http://2.bp.blogspot.com/-mASst_vcqCw/Urduz8c3uPI/AAAAAAAAarQ/0eS6QtSH3JI/s1600/John-Paul-Jones-Led-Zeppelin-Rolex-GMT-Master-Two-Tone.jpg",
-  lifespan: {
-    birth: 1961,
-    death: 2222
-  }
-},
-{
-  title: "Drummer",
-  name: "John Bonham",
-  bio: "Banging on drums while sporting a heafty stache is how he rolled.",
-  image: "http://www.drummagazine.com/images/lessons/120711-Bonzo-B&W.jpg",
-  lifespan: {
-    birth: 1948,
-    death: 1980
-  }
-}
-];
+var zepp = [{
+    title: "Lead Guitar",
+    name: "Jimmy Page",
+    bio: "Playing guitar while a acitve member in the cult of pony tail enthusiast.",
+    image: "http://www.theglobeandmail.com/arts/music/article25802019.ece/BINARY/w940/image.jpg",
+    lifespan: {
+        birth: 1957,
+        death: 2345
+    }
+}, {
+    title: "Lead Singer",
+    name: "Robert Plant",
+    bio: "Singing classic rock songs and wearing shirts that shows a bounty of chest hair.",
+    image: "http://static.tumblr.com/6741800edc5bac8e1649df8b2901420d/a38zs7m/019mx47u2/tumblr_static_robert_plant02.jpg",
+    lifespan: {
+        birth: 1965,
+        death: 2200
+    }
+}, {
+    title: "Bass Player",
+    name: "John Paul Jones",
+    bio: "Rockin the bass and a Lord of the Rings extra.",
+    image: "http://2.bp.blogspot.com/-mASst_vcqCw/Urduz8c3uPI/AAAAAAAAarQ/0eS6QtSH3JI/s1600/John-Paul-Jones-Led-Zeppelin-Rolex-GMT-Master-Two-Tone.jpg",
+    lifespan: {
+        birth: 1961,
+        death: 2222
+    }
+}, {
+    title: "Drummer",
+    name: "John Bonham",
+    bio: "Banging on drums while sporting a heafty stache is how he rolled.",
+    image: "http://www.drummagazine.com/images/lessons/120711-Bonzo-B&W.jpg",
+    lifespan: {
+        birth: 1948,
+        death: 1980
+    }
+}];
 
 var myDisplay = document.getElementById('person');
 var counter = 0;
@@ -47,51 +42,47 @@ var myArray;
 var contentToDisplay = "";
 
 for (; counter < zepp.length; counter++) {
-  myArray = zepp[counter];
-  // Give each person element a unique identifier
-  contentToDisplay += `<div class="person__container" id="person--${counter}">`;
-  contentToDisplay += `<div class= "title">${myArray.title}</div>`;
+    myArray = zepp[counter];
+    contentToDisplay += `<div class="person__container" id="person--${counter}">`;
+    contentToDisplay += `<div class= "title">${myArray.title}</div>`;
 
-  contentToDisplay += `<div class= "name">${myArray.name}</div>`;
+    contentToDisplay += `<div class= "name">${myArray.name}</div>`;
 
-  contentToDisplay += `<div class= "bio">${myArray.bio}</div>`;
+    contentToDisplay += `<div class= "bio">${myArray.bio}</div>`;
 
-  contentToDisplay += `<div class= "image"> src="${myArray.image}"</div>`;
+    contentToDisplay += `<div class= "image"> src="${myArray.image}"</div>`;
 
-  contentToDisplay += "Birth:" + `<div class= "lifespan">${myArray.lifespan.birth}</div>`;
+    contentToDisplay += "Birth:" + `<div class= "lifespan">${myArray.lifespan.birth}</div>`;
 
-  contentToDisplay += "Death:" + `<div class= "lifespan">${myArray.lifespan.death}</div>`;
+    contentToDisplay += "Death:" + `<div class= "lifespan">${myArray.lifespan.death}</div>`;
 
-  contentToDisplay += "</div>";
-  myDisplay.innerHTML = contentToDisplay;
+    contentToDisplay += "</div>";
+    myDisplay.innerHTML = contentToDisplay;
 };
-// Now containerEl will have elements in it
+
 var containerEl = document.getElementsByClassName("person__container");
 var inputBox = document.getElementById('input');
 var targetToEdit;
-// Event listeners are created
+
 for (var i = 0; i < containerEl.length; i++) {
-  var targetEmt = containerEl[i];
-  targetEmt.addEventListener("click", function(event) {
-    // Logic to execute when the element is clicked
-    this.classList.toggle("border");
-    inputBox.focus();
-    // logic for mirror inputBox
-    inputBox.value = this.childNodes[2].innerHTML;
-    targetToEdit = this;
-    inputBox.addEventListener("keyup", textInput)
-  });
-  targetEmt.addEventListener("mouseout", function(event) {
-    // Logic to execute when the element is clicked
+    var targetEmt = containerEl[i];
+    targetEmt.addEventListener("click", function(event) {
+        this.classList.toggle("border");
+        inputBox.focus();
+        inputBox.value = this.childNodes[2].innerHTML;
+        targetToEdit = this;
+        inputBox.addEventListener("keyup", textInput)
+    });
+    targetEmt.addEventListener("mouseout", function(event) {
 
-    inputBox.removeEventListener("keyup", textInput);
+        inputBox.removeEventListener("keyup", textInput);
 
-  });
+    });
 };
 
 function textInput(e) {
-  targetToEdit.childNodes[2].innerHTML = inputBox.value;
-  if (e.keyCode === 13) {
-    inputBox.removeEventListener("keyup", textInput);
-  }
+    targetToEdit.childNodes[2].innerHTML = inputBox.value;
+    if (e.keyCode === 13) {
+        inputBox.removeEventListener("keyup", textInput);
+    }
 }
